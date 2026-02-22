b52b048e80c214d4b641ffa2ea794f09e90a0e07
hack16hive <hritika1602@gmail.com>
2026-02-22 16:45:23 +0530

    renamed hritika.java to hritikaUC1.java

--- hritikaUC1.java
+++ hritikaUC1.java
@@ -3,7 +3,7 @@
  * UC1: Print the literal text "OOPS" to the console.
  * @author HritikaSingh
  * @version 1.0 **/
-public class hritika
+public class hritikaUC1
 {
     public static void main(String[] args) {
         System.out.println("OOPS");
