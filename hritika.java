8f5bc2f6e58267e0ddec0ef759a5a7a0d9ed224c
hack16hive <hritika1602@gmail.com>
2026-02-22 15:47:48 +0530

    UC1: PrintOOPS - initial implementation

---
+++ hritika.java
@@ -0,0 +1,10 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print the literal text "OOPS" to the console.**/
+public class hritika
+{
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+    
+}
