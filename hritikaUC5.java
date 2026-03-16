ca09d5afbbb9f9ff0ac1d01b3c23c5392947b465
hack16hive <hritika1602@gmail.com>
2026-03-16 20:20:03 +0530

    array initialisation

---
+++ hritikaUC5.java
@@ -0,0 +1,18 @@
+public class hritikaUC5 {
+    public static void main(String[] args) {
+
+        String[] lines = {
+            " ***    ***    *****   ***** ",
+            "*   *  *   *   *   *   *     ",
+            "*   *  *   *   *   *   *     ",
+            "*   *  *   *   *****   ***** ",
+            "*   *  *   *   *           * ",
+            "*   *  *   *   *           * ",
+            " ***    ***    *       ***** "
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
